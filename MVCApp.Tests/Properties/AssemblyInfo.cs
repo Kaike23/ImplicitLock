748425a2651fd70cd54b151ea04ee4879c1f3ac5
Kaike23 <zbarrera@codeservices.com.mx>
2015-09-14 10:47:26 -0700

    Initial State
    No Lock implementation, "clean" project structure.

---
+++ MVCApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99bb6b0e-908f-421e-8b20-abacf2732ec0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Respository/CustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Repository
+{
+    using Infrastructure.Mapping;
+    using Infrastructure.UnitOfWork;
+    using Model.Customers;
+    using Repository.Base;
+
+    public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(IUnitOfWork uow, IDataMapper<Customer> mapper) : base(uow, mapper) { }
+
+        protected override string TableName { get { return "Customers"; } }
+    }
+}
+
